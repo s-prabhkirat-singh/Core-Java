637b40745b3b025fbe066938bd8aa19e7f0dd328
Prabhkirat Singh <prabhkiratsingh2278@gmail.com>
2023-08-03 14:14:29 +0530

    Add files via upload

---
+++ Ternary_operator.java
@@ -0,0 +1,10 @@
+// This java file contains the working of ternary operator
+public class Ternary_operator{
+    public static void main(String[] args) {
+        // It operates on 3 operands
+        int x=5;
+        int y= 4==x?1:0;
+        System.out.println(y);
+        System.out.println("hey");
+    }
+}
